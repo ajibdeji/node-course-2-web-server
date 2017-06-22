6e1a24411d796a8c9081ceb60f050aef221039e5
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-06-22 09:22:11 -0400

    setup start script and heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -15,9 +15,7 @@ app.use((req, res, next) => {
     fs.appendFileSync(__dirname + '/log.txt', log + '\n');
     next();
 });
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
@@ -36,5 +34,5 @@ app.get('/about', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('server is up on port 3000');
+    console.log(`server is up on port ${port}`);
 });
